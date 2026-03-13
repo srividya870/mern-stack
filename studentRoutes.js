b28513806427533817608caf3d5b380aca59cb83
srividya870 <ammuramesh870@gmail.com>
2026-03-13 12:33:35 +0530

    creating route

---
+++ studentRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const Student = require("../models/student");
+
+
+router.post("/students",async(req, res) =>{
+    const student = new Student(req.body);
+    await student.save();
+    res.send(student);
+});
+
+router.get("/students",async(req,res) => {
+    const students = await Student.find();
+    res.send(students);
+});
+
+module.exports = router;
