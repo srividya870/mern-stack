61222f59893987bd6f50cd5583dcd9c06d303f01
srividya870 <ammuramesh870@gmail.com>
2026-03-13 12:41:04 +0530

    facultyroute

---
+++ facultyRoutes.js
@@ -0,0 +1,20 @@
+  
+    const express = require("express");
+const router = express.Router();
+const Faculty = require("../models/Faculty");
+
+
+router.post("/faculty", async (req, res) => {
+    const faculty = new Faculty(req.body);
+    await faculty.save();
+    res.send(faculty);
+});
+
+
+router.get("/faculty", async (req, res) => {
+    const faculty = await Faculty.find();
+    res.send(faculty);
+});
+
+module.exports = router;
+
